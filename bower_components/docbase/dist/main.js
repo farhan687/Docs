2299903a75e681f768ca3dd5a2b032e205831c36
Siddharth Kothari <sids.aquarius@gmail.com>
2015-10-27 03:35:52 -0500

    update bower components

--- bower_components/docbase/dist/main.js
+++ bower_components/docbase/dist/main.js
@@ -1,3 +1,5 @@
+
+// FILE: scripts/docbase.js
 /**
  *
  * Docbase engine
@@ -623,3 +625,258 @@
         return lastIndex !== -1 && lastIndex === position;
     }
 })(window.jQuery, window.angular);
+
+
+// FILE: scripts/docbase.min.js
+!function(t,e){function n(t){return e.isString(t)?(t="/"===t.charAt(0)?t.substring(1):t,a(t,"/")?t.substring(0,t.length-1):t):t}function r(t){return schema({"*":Array.of(schema({name:String,label:String}))})(t)}function o(e,n){var r=e.path,o=r.split("/"),i=o.splice(o.length-1,1);o.join("/"),i=i[0];var l="https://api.github.com/repos/"+e.user+"/"+e.repo+"/",c=l+"contents/"+o;t.get(c,{ref:e.branch}).success(function(e){var r=e.filter(function(t){return t.name===i})[0].sha;t.get(l+"git/trees/"+r+"?recursive=1").success(function(t){t=t.tree.filter(function(t){return a(t.path,".md")});var e={};t.forEach(function(t){var n=t.path.split("/");if(n.length>=3){var r=n[0],o=n[1],a=n[2].substring(0,n[2].length-3);e[r]||(e[r]=[]),e[r].filter(function(t){return t.name===o}).length||e[r].push({label:o,name:o,files:[]}),e[r].forEach(function(t){t.name===o&&t.files.push({name:a,label:a})})}}),n(null,e)}).error(function(t){n(t)})}).error(function(t){n(t)})}function a(t,e,n){(void 0===n||n>t.length)&&(n=t.length),n-=e.length;var r=t.indexOf(e,n);return-1!==r&&r===n}var i,l=t(window),c=this,s=c.Docbase={},u=s.events={},h=s.route={};s.methods=["file","github","generic"],s.run=function(r){var o={method:"github",map:{file:"map.json",path:""},file:{path:"docs"},github:{path:"/",branch:"gh-pages",editGithubBtn:!0},generic:{baseurl:"",path:"/"},html5mode:!1,indexType:"html",indexSrc:"v1/path/index.md",indexHtml:"html/main.html",flatdocHtml:"html/flatdoc.html",angularAppName:"docbaseApp"};if(r=t.extend({},o,r),!("github"!==r.method||r.github.user&&r.github.repo))throw"Missing GitHub user/repo info.";s.methods.forEach(function(t){var e=r[t];Object.keys(e).forEach(function(t){e[t]=n(e[t])})}),r.map.path=n(r.map.path),s.options=r,u.bind(),i=e.module(r.angularAppName,["ngRoute"]).factory("FlatdocService",["$q","$route","$location","$anchorScroll","$http",h.fetch]).controller("URLCtrl",["$scope","$location","$filter","data","commits",h.URLCtrl]).controller("MainCtrl",["$scope","$location","$timeout",h.mainCtrl]).config(["$routeProvider","$locationProvider",h.config]).run(["$rootScope","$location","$routeParams","$anchorScroll","$route",h.anchorConfig]),"file"===r.method?s.file(r.map):"github"===r.method?s.github(r.github):s.file(r.map)},s.github=function(e){o(e,function(e,n){if(e)s.file(s.options.map);else{if(!r(n))throw"GitHub tree mapping error.";s.map=n,t.get(s.options.map.path+"/"+s.options.map.file).success(function(t){var e=s.map,n=Object.keys(t);n.forEach(function(n){e[n]&&e[n].forEach(function(e){var r=t[n].filter(function(t){return t.name===e.name})[0];r&&r.label&&(e.label=r.label,e.files.forEach(function(t){var e=r.files.filter(function(e){return e.name===t.name})[0];e&&e.label&&(t.label=e.label)}))})}),l.trigger("mapped"),u.bind()}).error(function(){l.trigger("mapped"),u.bind()})}})},s.file=s.generic=function(e){t.get(e.path+"/"+e.file).success(function(t){if(!r(t))throw"Map file schema error. Check the documentation.";var e=Object.keys(t);if(!(e.length&&t[e[0]][0].files.length&&t[e[0]][0].files[0].name))throw"Map does not have a file entry. Check the documentation";s.map=t,l.trigger("mapped"),u.bind()}).error(function(t){throw t})},u.switchBind=function(t){l[t]("flatdoc:ready",u.ready),l[t]("ajaxError",u.ajaxError)},u.bind=function(){u.switchBind("on")},u.unbind=function(){u.switchBind("off")},u.ready=function(){l.trigger("docbase:ready")},u.ajaxError=function(t,e){403===e.status&&"github"===s.options.method&&console.error("Github API quota exceeded.")},h.config=function(t,e){var n=s.options.flatdocHtml,r=s.options.indexHtml,o={data:function(t){return t.getData().then(function(t){return t})},commits:function(t){return t.getCommits()}};t.when("/:version/:folder/:file",{templateUrl:n,controller:"URLCtrl",resolve:o}).when("/:version/:folder",{templateUrl:n,controller:"URLCtrl",resolve:o}).when("/:version",{templateUrl:n,controller:"URLCtrl",resolve:o}).when("/",{templateUrl:r,controller:"MainCtrl"}).otherwise({redirectTo:"/"}),e.html5Mode(s.options.html5mode)},h.anchorConfig=function(e,n,r,o,a){e.$on("$locationChangeStart",function(t,r,i){var l=r.split("#"),c=l[l.length-1];l.splice(l.length-1,1),l=l.join("#");var s=i.split("#");if(s.splice(2===s.length?2:s.length-1,1),s=s.join("#"),l===s&&r!==i){n.hash(c);var u=a.current,h=e.$on("$locationChangeSuccess",function(){a.current=u,h()});o()}}),e.$on("$routeChangeSuccess",function(){l.on("docbase:ready",function(){o(),t(".content").find("pre code").each(function(){t(this).addClass("prettyprint")}),prettyPrint()})})},h.fetch=function(e,n,r,o,a){function i(){function r(){var e=s.map,r={},a=n.current.params.version,i=Object.keys(e),l=h.updatePath(n.current.params);if(r.versions=i,r.currentVersion=a||i[i.length-1],r.map=e,r.locationPath=l.path,l.fail)r.github=!1,o.resolve(r);else{var c=s.options,f=s.options.github,p="https://github.com/"+f.user+"/"+f.repo+"/tree/"+f.branch+"/"+f.path+l.path+".md",d=c.file.path+l.path+".md";"github"===c.method?d="https://raw.githubusercontent.com/"+f.user+"/"+f.repo+"/"+f.branch+"/"+f.path+l.path+".md":"generic"===c.method&&(d=c.generic.baseurl+"/"+c.generic.path+l.path+".md"),r.github=p,u.parsed=!1,console.log(d),Flatdoc.file(d)(function(e,n){n=n.split("\n");var a=n.shift();a=a.replace(/\u201D/g,'"'),a=a.replace(/\u201C/g,'"');try{a=JSON.parse(a)}catch(i){n.unshift(a),a={threeColumns:!1}}n=n.join("\n"),a.threeColumns?t("body").removeClass("no-literate"):t("body").addClass("no-literate"),a.bigh3?t("body").addClass("big-h3"):t("body").removeClass("big-h3"),a.largeBrief?t("body").addClass("large-brief"):t("body").removeClass("large-brief");var l=Flatdoc.parser.parse(n,function(t){return Flatdoc.highlighters.generic(t)});r.markdown=l,o.resolve(r)})}}{var o=e.defer();s.options}return s.map?r():l.on("mapped",r),o.promise}return{getData:function(){return new i},getCommits:function(){var t=s.options,e=n.current.params,r=t.github.path+"/"+e.version+"/"+e.folder+"/"+e.file;return a.get("https://api.github.com/repos/"+t.github.user+"/"+t.github.repo+"/commits?path="+r+".md")}}},h.URLCtrl=function(e,n,r,o,a){if(n.path(o.locationPath),!o.fail){e.versions=o.versions,e.currentVersion=o.currentVersion,e.map=o.map,e.github=o.github;var i=o.markdown,c=[];t('[role="flatdoc-content"]').html(i.content.find(">*")),t('[role="flatdoc-menu"]').html(Flatdoc.menuView(i.menu)),l.trigger("flatdoc:ready")}var s=t("<div>").addClass("extra_container");if(200==a.status){for(var u=a.data,h=r("date")(a.data[0].commit.committer.date,"mediumDate"),f=t("<span>").addClass("pull-right modified-date").html('Last Modified On : <a href="'+a.data[0].html_url+'">'+h+"</a>"),p=u,d=t("<div>").addClass("contributor-container"),m=0;m<p.length;m++){var g=p[m].committer;if(-1==jQuery.inArray(g.login,c)){c.push(g.login);var b=t("<img>").addClass("contributor_img img-rounded").attr({src:g.avatar_url,alt:g.login}),v=t("<a>").addClass("contributor").attr({href:g.html_url,title:g.login,target:"_blank"}).append(b);d.append(v)}}var C=t("<div>").addClass("contributors_header").append("Contributors").append(f);t(s).prepend(d).prepend(C)}var y=t("<div>").addClass("clearFix");t('[role="flatdoc-content"]').prepend(y).prepend(s)},h.mainCtrl=function(t,e,n){if("markdown"===s.options.indexType){var r=s.options.indexSrc;a(r,".md")&&(r=r.substring(0,r.length-3)),"/"!==r.charAt(0)&&(r="/"+r),e.path(r)}else{var o=function(){n(function(){t.map=s.map,t.versions=Object.keys(t.map),t.currentVersion=t.versions[0]})};s.map?o():l.on("mapped",o)}},h.updatePath=function(t){var e=s.map,n=t.version,r=t.folder,o=t.file;if(!e[n])return console.error("Version not mapped."),{path:"/",fail:!0};var a;if(r&&(a=e[n].filter(function(t){return t.name===r}),!a.length))return console.error("Folder not mapped."),{path:"/"+n,fail:!0};if(o){var i=a[0].files.filter(function(t){return t.name===o});if(!i.length)return console.error("File not mapped."),{path:"/"+n+"/"+o,fail:!0}}r=r||e[n][0].name;var l=e[n].filter(function(t){return t.name===r})[0];o=o||l.files&&l.files[0].name;var c="/"+n+"/"+r+"/"+o;return"undefined"==typeof o&&(c="/"+n+"/"+r),{path:c,fail:!1}}}(window.jQuery,window.angular);
+
+// FILE: scripts/flatdoc-theme.js
+/**
+ * Official flatdoc theme
+ * modified
+ */
+
+(function($) {
+  var $window = $(window);
+  var $document = $(document);
+
+  $window.on('docbase:ready', runTheme);
+
+  function runTheme() {
+
+    $("h2, h3").scrollagent({
+      offset: 100
+    }, function(cid, pid, currentElement, previousElement) {
+      if (pid) {
+        $("[pref='#" + pid + "']").removeClass('active');
+      }
+      if (cid) {
+        $("[pref='#" + cid + "']").addClass('active');
+      }
+    });
+
+    $('.menu a').each(function() {
+      var el = $(this);
+      var href = el.attr('href');
+
+      if (href && !el.attr('pref')) {
+        var location = window.location.href.split('#');
+        if (location.length <= 2) {
+          location = location.join('#') + href;
+        } else {
+          location[location.length - 1] = href.substring(1);
+          location = location.join('#');
+        }
+        el.attr('href', location);
+        el.attr('pref', href);
+      }
+    });
+
+    var $sidebar = $('.menubar');
+    var elTop;
+
+    $window
+      .on('resize.sidestick', function() {
+        $sidebar.removeClass('fixed');
+        elTop = $sidebar.offset().top;
+        $window.trigger('scroll.sidestick');
+      })
+      .on('scroll.sidestick', function() {
+        var scrollY = $window.scrollTop();
+        $sidebar.toggleClass('fixed', (scrollY >= elTop - 35));
+      })
+      .trigger('resize.sidestick');
+
+  }
+
+})(window.jQuery);
+
+
+/*! jQuery.scrollagent (c) 2012, Rico Sta. Cruz. MIT License.
+ *  https://github.com/rstacruz/jquery-stuff/tree/master/scrollagent */
+
+// Call $(...).scrollagent() with a callback function.
+//
+// The callback will be called everytime the focus changes.
+//
+// Example:
+//
+//      $("h2").scrollagent(function(cid, pid, currentElement, previousElement) {
+//        if (pid) {
+//          $("[href='#"+pid+"']").removeClass('active');
+//        }
+//        if (cid) {
+//          $("[href='#"+cid+"']").addClass('active');
+//        }
+//      });
+
+(function($) {
+
+  $.fn.scrollagent = function(options, callback) {
+    var $window = $(window);
+
+    // Account for $.scrollspy(function)
+    if (typeof callback === 'undefined') {
+      callback = options;
+      options = {};
+    }
+
+    var $sections = $(this);
+    var $parent = options.parent || $(window);
+
+    // Find the top offsets of each section
+    var offsets = [];
+    $sections.each(function(i) {
+      var offset = ($(this).attr('data-anchor-offset') ?
+        parseInt($(this).attr('data-anchor-offset'), 10) :
+        (options.offset || 0));
+
+      offsets.push({
+        id: $(this).attr('id'),
+        index: i,
+        el: this,
+        offset: offset
+      });
+    });
+
+    // State
+    var current = null;
+    var height = null;
+    var range = null;
+
+    // Save the height. Do this only whenever the window is resized so we don't
+    // recalculate often.
+    function refreshSize() {
+      height = $parent.height();
+      range = $(document).height();
+    }
+
+    // Find the current active section every scroll tick.
+    function refreshScroll() {
+      var y = $parent.scrollTop();
+      y += height * (0.3 + 0.7 * Math.pow(y / range, 2));
+
+      var latest = null;
+
+      for (var i in offsets) {
+        if (offsets.hasOwnProperty(i)) {
+          var offset = offsets[i];
+          if ($(offset.el).offset().top + offset.offset < y) latest = offset;
+        }
+      }
+
+      if (latest && (!current || (latest.index !== current.index))) {
+        callback.call($sections,
+          latest ? latest.id : null,
+          current ? current.id : null,
+          latest ? latest.el : null,
+          current ? current.el : null);
+        current = latest;
+      }
+    }
+
+    $window.on('scroll', $.throttle(50, refreshScroll));
+    $window.on('resize', $.throttle(250, refreshSize));
+
+    refreshSize();
+    refreshScroll();
+
+    return this;
+  };
+
+})(jQuery);
+
+/*
+ * jQuery throttle / debounce - v1.1 - 3/7/2010
+ * http://benalman.com/projects/jquery-throttle-debounce-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function(b, c) {
+  var $ = b.jQuery || b.Cowboy || (b.Cowboy = {}),
+    a;
+  $.throttle = a = function(e, f, j, i) {
+    var h, d = 0;
+    if (typeof f !== "boolean") {
+      i = j;
+      j = f;
+      f = c;
+    }
+
+    function g() {
+      var o = this,
+        m = +new Date() - d,
+        n = arguments;
+
+      function l() {
+        d = +new Date();
+        j.apply(o, n);
+      }
+
+      function k() {
+        h = c;
+      }
+      if (i && !h) {
+        l();
+      }
+      h && clearTimeout(h);// jshint ignore:line
+      if (i === c && m > e) {
+        l();
+      } else {
+        if (f !== true) {
+          h = setTimeout(i ? k : l, i === c ? e - m : e);
+        }
+      }
+    }
+    if ($.guid) {
+      g.guid = j.guid = j.guid || $.guid++;
+    }
+    return g;
+  };
+  $.debounce = function(d, e, f) {
+    return f === c ? a(d, e, false) : a(d, f, e !== false);
+  };
+})(this);
+
+// FILE: scripts/polyfill.js
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function bind(that) { // .length is 1
+        var slice = [].slice;
+        var Empty = function() {};
+        var target = this;
+        if (typeof target != "function") {
+            throw new TypeError("Function.prototype.bind called on incompatible " + target);
+        }
+        var args = slice.call(arguments, 1); // for normal call
+        var bound = function() {
+
+            if (this instanceof bound) {
+
+                var result = target.apply(
+                    this,
+                    args.concat(slice.call(arguments))
+                );
+                if (Object(result) === result) {
+                    return result;
+                }
+                return this;
+
+            } else {
+                return target.apply(
+                    that,
+                    args.concat(slice.call(arguments))
+                );
+
+            }
+
+        };
+        if (target.prototype) {
+            Empty.prototype = target.prototype;
+            bound.prototype = new Empty();
+            Empty.prototype = null;
+        }
+        return bound;
+    };
+}
