6df1854c8d55afa415cbcf0a46bacc7d610bda55
Farhan Chauhan <farhan687@gmail.com>
2015-11-12 12:02:22 +0530

    Search box style & events

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,7 @@
 (function ( $ ) {
     $.fn.searchAppbase = function() {
     	var $search = this;
+		$search.addClass('appbase-search');
 		$.get('https://raw.githubusercontent.com/appbaseio/Docs/gh-pages/search-index.json').done(function(data){
 			onSuccessData(data);
 		});
@@ -14,12 +15,33 @@
 			});
 
 			$search.typeahead({
-			    highlight: true
+			    minLength: 1
 			}, {
 			    name: 'titles',
 			    displayKey: 'title',
-			    source:posts
+			    source:posts,
+			    templates: {
+					pending: true,
+					suggestion: function(data) {
+						if (data) {
+							var single_record = search_tag(data);
+							return single_record;
+						} else
+							return;
+					}
+				}
 			});
+	    	
+	    	$search.bind('typeahead:select', function(ev, suggestion) {
+			  window.location.href = suggestion.link;
+			});
+
+	    }
+
+	    function search_tag(data){
+	    	var result_a = $('<a>').addClass('result_record_a').attr('href',data.link).text(data.title);
+	    	var result_div = $('<div>').addClass('result_record').append(result_a);
+	    	return result_div;
 	    }		    
 }
 }( jQuery ));
